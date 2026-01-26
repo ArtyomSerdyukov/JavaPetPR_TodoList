9d37f418357d411904a2acca06a8106e9c2865a1
Сердюков Артем <artem2234320071974@mail.ru>
2026-01-27 01:42:00 +0300

    finished_commit

---
+++ src/main/java/com/app/todoapp/models/Task.java
@@ -0,0 +1,19 @@
+package com.app.todoapp.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Entity
+@Data
+@Table(name = "task")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private boolean completed;
+
+
+}
+
